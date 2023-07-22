aaa0a6989aba234e2e69a67439f6b050c951e66c
Den <no.den2018@yandex.ru>
2023-07-22 14:09:28 +0500

    Выполнили второе дз

---
+++ task36/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+void GetSumOddNumbers(int [] array)
+{
+    int sumOddNumbers = 0;
+    for(int i = 0; i < array.Length; i ++)
+    {
+        if(i % 2 == 1)
+        sumOddNumbers +=array[i];
+    }
+    Console.WriteLine("Сумма нечётных позиций массива, равна: " + sumOddNumbers);
+}
+
+int[] GetArray()
+{
+    int [] newArray = new int [8];
+    Random rand = new Random();
+    for(int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = rand.Next(0, 50);
+    }
+    return newArray;
+}
+
+int [] array = GetArray();
+Console.Write($"[{String.Join(",", array)}]");
+Console.WriteLine();
+ GetSumOddNumbers(array);
